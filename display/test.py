f6399c8d2e8de15c8a224fdf4d7293ebf56dce15
이동복 <typoon0820@gmail.com>
2024-11-18 15:06:49 +0900

    tst

--- display/test.py
+++ display/test.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import sys
 import time
 from PyQt4.QtGui import QApplication, QLabel, QHBoxLayout, QVBoxLayout, QMainWindow, QWidget, QPushButton, QPixmap, QFont, QFontDatabase
