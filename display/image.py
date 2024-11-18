f266a16f0f2546f95652d2530d1b9c7b1927df64
이동복 <typoon0820@gmail.com>
2024-11-19 00:46:27 +0900

    t

--- display/image.py
+++ display/image.py
@@ -3,11 +3,9 @@ import os
 
 def download_image(image_url, save_dir="images", file_name="image.png"):
 
-    # 디렉토리 생성
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     
-    # 이미지 요청
     response = requests.get(image_url, stream=True)
     if response.status_code == 200:
         image_path = os.path.join(save_dir, file_name)
