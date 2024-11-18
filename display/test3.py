3ca403d7b2f89b0439aeb8933cc38545a7bb0ece
이동복 <typoon0820@gmail.com>
2024-11-19 04:12:40 +0900

    t

--- display/test3.py
+++ display/test3.py
@@ -4,3 +4,7 @@ data = read.read_token()
 data2 = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqaW5naTY0NjJAZ21haWwuY29tIiwicm9sZSI6IlJPTEVfVVNFUiIsImlkIjoyNywiaXNzIjoiU1NtYXJ0T2ZmaWNlIiwiaWF0IjoxNzMxOTU2NTM3LCJleHAiOjE3MzMyNTI1Mzd9.j1ZmkUMG6dwM4IJzbu41qIPIlOFldTniraU5_E0BITY"
 
 print(data == data2)
+
+print(len(data))
+print(len(data2))
+
