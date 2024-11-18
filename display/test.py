831683d8ff60e26199d8895985125528f46210e0
이동복 <typoon0820@gmail.com>
2024-11-18 22:53:06 +0900

    3

--- display/test.py
+++ display/test.py
@@ -59,6 +59,62 @@ class MainWindow(QMainWindow):
         # 레이아웃 설정
         main_layout = QHBoxLayout(self.central_widget)
 
+        # 사진 영역
+        self.photo_label = QLabel()
+        self.photo_label.setPixmap(
+            QPixmap("user_photo.jpg").scaled(280, 280, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        )
+        self.photo_label.setAlignment(Qt.AlignCenter)
+
+        # 정보 영역
+        info_layout = QVBoxLayout()
+
+        # 이름
+        self.name_label = QLabel(unicode(user_name,'utf-8'))
+        self.name_label.setFont(QFont(nanumB_font, 18, QFont.Bold))
+        self.name_label.setStyleSheet("color: #333333;")
+        self.name_label.setAlignment(Qt.AlignCenter)
+
+        # 사원번호
+        self.id_label = QLabel(unicode("사원번호: " + user_employee_number,'utf-8'))
+        self.id_label.setFont(QFont(nanumB_font, 12))
+        self.id_label.setStyleSheet("color: #555555;")
+        self.id_label.setAlignment(Qt.AlignCenter)
+
+        # 팀 이름
+        self.team_label = QLabel(unicode("직급: " + user_duty,'utf-8'))
+        self.team_label.setFont(QFont(nanumB_font, 12))
+        self.team_label.setStyleSheet("color: #555555;")
+        self.team_label.setAlignment(Qt.AlignCenter)
+
+        # 직책
+        self.position_label = QLabel(unicode("직책: " + user_position,'utf-8'))
+        self.position_label.setFont(QFont(nanumB_font, 12))
+        self.position_label.setStyleSheet("color: #555555;")
+        self.position_label.setAlignment(Qt.AlignCenter)
+
+        # 전화번호
+        self.phone_label = QLabel(unicode("전화번호: " + user_phone_number,'utf-8'))
+        self.phone_label.setFont(QFont(nanumB_font, 12))
+        self.phone_label.setStyleSheet("color: #555555;")
+        self.phone_label.setAlignment(Qt.AlignCenter)
+
+        # 이메일
+        self.email_label = QLabel(unicode("이메일: " + user_email,'utf-8'))
+        self.email_label.setFont(QFont(nanumB_font, 12))
+        self.email_label.setStyleSheet("color: #555555;")
+        self.email_label.setAlignment(Qt.AlignCenter)
+
+        # 정보 레이아웃에 추가
+        info_layout.addWidget(self.name_label)
+        info_layout.addWidget(self.id_label)
+        info_layout.addWidget(self.team_label)
+        info_layout.addWidget(self.position_label)
+        info_layout.addWidget(self.phone_label)
+        info_layout.addWidget(self.email_label)
+
+        self.layout.addWidget(self.photo_label, alignment=Qt.AlignLeft)  # 사진 왼쪽 정렬
+        self.layout.addLayout(info_layout)
 
         # 최상위 이미지 레이블 생성
         self.image_label = QLabel(self)
@@ -85,63 +141,12 @@ class MainWindow(QMainWindow):
         """이미지를 숨기거나 보이게 하는 함수."""
         if self.image_visible:
             self.image_label.hide()  # 이미지 숨기기
-            
-            # 사진 영역
-            self.photo_label = QLabel()
-            self.photo_label.setPixmap(
-                QPixmap("user_photo.jpg").scaled(280, 280, Qt.KeepAspectRatio, Qt.SmoothTransformation)
-            )
-            self.photo_label.setAlignment(Qt.AlignCenter)
-
-            # 정보 영역
-            info_layout = QVBoxLayout()
-
-            # 이름
-            self.name_label = QLabel(unicode(user_name,'utf-8'))
-            self.name_label.setFont(QFont(nanumB_font, 18, QFont.Bold))
-            self.name_label.setStyleSheet("color: #333333;")
-            self.name_label.setAlignment(Qt.AlignCenter)
-
-            # 사원번호
-            self.id_label = QLabel(unicode("사원번호: " + user_employee_number,'utf-8'))
-            self.id_label.setFont(QFont(nanumB_font, 12))
-            self.id_label.setStyleSheet("color: #555555;")
-            self.id_label.setAlignment(Qt.AlignCenter)
-
-            # 팀 이름
-            self.team_label = QLabel(unicode("직급: " + user_duty,'utf-8'))
-            self.team_label.setFont(QFont(nanumB_font, 12))
-            self.team_label.setStyleSheet("color: #555555;")
-            self.team_label.setAlignment(Qt.AlignCenter)
-
-            # 직책
-            self.position_label = QLabel(unicode("직책: " + user_position,'utf-8'))
-            self.position_label.setFont(QFont(nanumB_font, 12))
-            self.position_label.setStyleSheet("color: #555555;")
-            self.position_label.setAlignment(Qt.AlignCenter)
-
-            # 전화번호
-            self.phone_label = QLabel(unicode("전화번호: " + user_phone_number,'utf-8'))
-            self.phone_label.setFont(QFont(nanumB_font, 12))
-            self.phone_label.setStyleSheet("color: #555555;")
-            self.phone_label.setAlignment(Qt.AlignCenter)
-
-            # 이메일
-            self.email_label = QLabel(unicode("이메일: " + user_email,'utf-8'))
-            self.email_label.setFont(QFont(nanumB_font, 12))
-            self.email_label.setStyleSheet("color: #555555;")
-            self.email_label.setAlignment(Qt.AlignCenter)
-
-            # 정보 레이아웃에 추가
-            info_layout.addWidget(self.name_label)
-            info_layout.addWidget(self.id_label)
-            info_layout.addWidget(self.team_label)
-            info_layout.addWidget(self.position_label)
-            info_layout.addWidget(self.phone_label)
-            info_layout.addWidget(self.email_label)
-
-            self.layout.addWidget(self.photo_label, alignment=Qt.AlignLeft)  # 사진 왼쪽 정렬
-            self.layout.addLayout(info_layout)
+            self.name_label.setText(unicode("이름: " + user_data['name'], "utf-8"))
+            self.id_label.setText(unicode("사원번호: " + user_data['employee_number'], "utf-8"))
+            self.team_label.setText(unicode("직급: " + user_data['duty'], "utf-8"))
+            self.position_label.setText(unicode("직책: " + user_data['position'], "utf-8"))
+            self.phone_label.setText(unicode("전화번호: " + user_data['phone_number'], "utf-8"))
+            self.email_label.setText(unicode("이메일: " + user_data['email'], "utf-8"))
         else:
             self.image_label.show()  # 이미지 보이기
         self.image_visible = not self.image_visible
