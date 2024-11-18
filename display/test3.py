c83ba58806301a4ef6f2b5a15c91e28b3d7a3e15
이동복 <typoon0820@gmail.com>
2024-11-18 16:59:59 +0900

    3

--- display/test3.py
+++ display/test3.py
@@ -1,43 +1,13 @@
-import sys
+import os,sys
 from PyQt4 import QtGui
 
-class Example(QtGui.QWidget):
+app = QtGui.QApplication(sys.argv)
+window = QtGui.QMainWindow()
+window.setGeometry(0, 0, 400, 200)
 
-    def __init__(self):
-        super(Example, self).__init__()
-        self.initUI()
-
-    def initUI(self):
-        QtGui.QToolTip.setFont(QtGui.QFont('Test', 10))
-        self.setToolTip('This is a <b>QWidget</b> widget')
-
-        # Show  image
-        self.pic = QtGui.QLabel(self)
-        self.pic.setGeometry(10, 10, 800, 800)
-        self.pic.setPixmap(QtGui.QPixmap("/home/lpp/Desktop/image1.png"))
-
-        # Show button 
-        btn = QtGui.QPushButton('Button', self)
-        btn.setToolTip('This is a <b>QPushButton</b> widget')
-        btn.resize(btn.sizeHint())
-        btn.clicked.connect(self.fun)
-        btn.move(50, 50)
-
-
-        self.setGeometry(300, 300, 2000, 1500)
-        self.setWindowTitle('Tooltips')
-        self.show()
-
-    # Connect button to image updating 
-    def fun(self):
-        self.pic.setPixmap(QtGui.QPixmap( "/home/lpp/Desktop/image2.png"))
-
-def main():
-
-    app = QtGui.QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
+pic = QtGui.QLabel(window)
+pic.setGeometry(10, 10, 400, 100)
+#use full ABSOLUTE path to the image, not relative
+pic.setPixmap(QtGui.QPixmap(os.getcwd() + "/user_photo.png"))
+window.show()
+sys.exit(app.exec_())
