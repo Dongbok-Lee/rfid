0542525fbfcbabb89b0c636f5ab20f96d42da790
이동복 <typoon0820@gmail.com>
2024-11-19 04:10:26 +0900

    t

--- display/test3.py
+++ display/test3.py
@@ -1,26 +1,6 @@
-# 원본 데이터
-data = ('Response Data:', {
-    u'status': 200,
-    u'msg': u'내 정보 조회에 성공했습니다.',
-    u'data': {
-        u'duty': u'주임',
-        u'status': u'OFF_DUTY',
-        u'name': u'이동복',
-        u'phoneNumber': None,
-        u'userId': 29,
-        u'profileImageUrl': u'https://sfdssafy.s3.amazonaws.com/동복.pngd5863b2d-d43d-4989-9f15-0da9af50f956',
-        u'role': u'USER',
-        u'employeeNumber': u'S24000006',
-        u'position': u'개발자',
-        u'email': u'typoon0820@gmail.com'
-    }
-})
+import read
 
-print(data[1])
+data = read.read_token()
+data2 = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqaW5naTY0NjJAZ21haWwuY29tIiwicm9sZSI6IlJPTEVfVVNFUiIsImlkIjoyNywiaXNzIjoiU1NtYXJ0T2ZmaWNlIiwiaWF0IjoxNzMxOTU2NTM3LCJleHAiOjE3MzMyNTI1Mzd9.j1ZmkUMG6dwM4IJzbu41qIPIlOFldTniraU5_E0BITY"
 
-
-# 사용자 정보 변수에 담기
-
-
-# 확인
-print("Response Label:", str(data[1][u'data'][u'email']))
+print(data == data2)
