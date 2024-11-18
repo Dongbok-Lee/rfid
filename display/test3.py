84bcf5964c2250efb3a05b6204b6bd0e97be57b8
이동복 <typoon0820@gmail.com>
2024-11-19 05:19:30 +0900

    t

--- display/test3.py
+++ display/test3.py
@@ -2,7 +2,11 @@ import read
 
 data = read.read_token()
 data2 = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqaW5naTY0NjJAZ21haWwuY29tIiwicm9sZSI6IlJPTEVfVVNFUiIsImlkIjoyNywiaXNzIjoiU1NtYXJ0T2ZmaWNlIiwiaWF0IjoxNzMxOTU2NTM3LCJleHAiOjE3MzMyNTI1Mzd9.j1ZmkUMG6dwM4IJzbu41qIPIlOFldTniraU5_E0BITY"
-
+if data2 in data:
+    print("True")
+else:
+    print("False")
+     
 print(data == data2)
 
 print(len(data.strip()))
